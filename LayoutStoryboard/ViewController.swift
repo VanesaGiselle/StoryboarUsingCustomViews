80aa96298ffe1d3ac04e239017d4d679d5fd9cb9
Vanesa Korbenfeld <vkorbenfeld@gmail.com>
2022-01-19 17:52:59 -0300

    Initial commit

--- LayoutStoryboard/ViewController.swift
+++ LayoutStoryboard/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -17,3 +17,41 @@ class ViewController: UIViewController {
 
 }
 
+class FacebookButton: UIButton {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    func setupView() {
+        self.backgroundColor = .systemBlue
+        self.tintColor = .white
+        self.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17.0)
+    }
+}
+
+class EmailButton: UIButton {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    func setupView() {
+        self.backgroundColor = UIColor(red: 207/255.0, green: 150/255.0, blue: 200/255.0, alpha: 1)
+        self.tintColor = .white
+        self.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17.0)
+    }
+}
+
+class TwitterButton: UIButton {
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupView()
+    }
+    
+    func setupView() {
+        self.backgroundColor = UIColor(red: 47/255.0, green: 166/255.0, blue: 242/255.0, alpha: 1)
+        self.tintColor = .white
+        self.titleLabel?.font = UIFont.boldSystemFont(ofSize: 17.0)
+    }
+}
